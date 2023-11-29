1c06a5ebb85029fd6edbc326ba21889ed44be944
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-11-29 10:27:39 +0100

    START DAY 2

--- app.js
+++ app.js
@@ -1,98 +1,38 @@
-const express = require(`express`)
-// On construit une instance d'express
+const express = require("express")
 const app = express()
 const port = 3000
 
-const mockCoworkings = require(`./mock-coworkings`)
-
-const arrUsers = [
-  {
-    id: 12,
-    name: "Jacques",
-    age: 28,
-  },
-  {
-    id: 15,
-    name: "Jean-Pierre",
-    age: 52,
-  },
-  {
-    id: 6,
-    name: "Micheline",
-    age: 41,
-  },
-]
+const mockCoworkings = require("./mock-coworkings")
 
 const logger = (req, res, next) => {
   const now = new Date()
   const hours = now.getHours()
   const minutes = now.getMinutes()
-  console.log(`${hours}h${minutes} - ${req.url} DANS LOGGER`)
+  console.log(`${hours}h${minutes < 10 ? "0" + minutes : minutes} - ${req.url} DANS LOGGER`)
+
   next()
 }
 
 app.use(logger)
 
-// Execution de app.js
 app.get("/", (req, res) => {
-  res.send("Page d'accueil")
+  res.send("Hello World !")
 })
 
 app.get("/api/coworkings", (req, res) => {
-  res.send(`Il y a ${mockCoworkings.length} coworkings dans la liste !`)
+  // Afficher la phrase : Il y a ... coworkings dans la liste.
+  res.send(`Il y a ${mockCoworkings.length} coworkings dans la liste.`)
 })
 
 app.get("/api/coworkings/:id", (req, res) => {
-  const urlId = parseInt(req.params.id)
-  let coworkingId = mockCoworkings.find((cowork) => cowork.id === urlId)
-  coworkingId = coworkingId ? coworkingId.name : `Le coworking numéro ${urlId} n'existe pas`
+  let result = mockCoworkings.find((el) => el.id === parseInt(req.params.id))
 
-  res.send(coworkingId)
-})
-
-app.get("/names", (req, res) => {
-  // Une requête ne peut renvoyer qu'une seule et unique réponse
-  // D'abord, on crée une chaîne de caractère à partir des éléments du tableau, puis on la renvoie dans une réponse
-  // => Jacques et Jean-Pierre et Mathilde !
-  let sentence = ""
-  arrUsers.forEach((object) => {
-    sentence += object.name + " "
-  })
-  res.send(sentence + "!")
-})
-
-app.get("/names/:id", (req, res) => {
-  // console.log(parseInt(req.params.id))
-  // Implémenter le test pour sélectionner dans le tableau l'objet dont l'id correspond à l'id passé en paramètre d'url
-
-  const urlId = parseInt(req.params.id)
-  //... trouver le bon objet dans le tableau
-
-  //   let result = "not found"
-  //   for (let i = 0; i < arrUsers.length; i++) {
-  //     const element = arrUsers[i]
-  //     if (element.id === urlId) {
-  //       result = arrUsers[i].name
-  //       break
-  //     }
-  //   }
-
-  let result = arrUsers.find((el) => el.id === urlId)
-  //   if (!result) {
-  //     result = "not found"
-  //   } else {
-  //     result = result.name
-  //   }
-
-  result = result ? result.name : "not found"
-  // on peut résumer le test précédent en une condition ternaire
+  if (!result) {
+    result = `Aucun élément ne correspond à l'id n°${req.params.id}`
+  }
   res.send(result)
 })
 
-//Ecoute sur le port 3000
 app.listen(port, () => {
-  console.log(`Le port qui en train d'être écouté est le port numéro ${port}`)
+  console.log(`Example app listening on port ${port}`)
 })
-
-
-FIN JOUR 1
