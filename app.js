7449aa8eb977097c8a856ad36781a8f181f1444f
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-11-29 14:42:31 +0100

    Achieve coworkings post endpoint

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
 const express = require("express")
+const morgan = require("morgan")
 const app = express()
 const port = 3000
 
 const mockCoworkings = require("./mock-coworkings")
 
-const logger = (req, res, next) => {
-  const now = new Date()
-  const hours = now.getHours()
-  const minutes = now.getMinutes()
-  console.log(`${hours}h${minutes < 10 ? "0" + minutes : minutes} - ${req.url} DANS LOGGER`)
-
-  next()
-}
-
-app.use(logger)
+// middleware qui permet d'interpréter' le corps de ma requête (req.body)  en format json
+app.use(express.json())
+app.use(morgan("dev"))
 
 app.get("/", (req, res) => {
-  res.send("Hello World !")
+  res.json("Hello World !")
 })
 
 app.get("/api/coworkings", (req, res) => {
   // Afficher la phrase : Il y a ... coworkings dans la liste.
-  res.send(`Il y a ${mockCoworkings.length} coworkings dans la liste.`)
+  res.json(mockCoworkings)
 })
 
 app.get("/api/coworkings/:id", (req, res) => {
@@ -30,7 +24,23 @@ app.get("/api/coworkings/:id", (req, res) => {
   if (!result) {
     result = `Aucun élément ne correspond à l'id n°${req.params.id}`
   }
-  res.send(result)
+  res.json(result)
+})
+
+// implémenter le endpoint post qui renvoie une réponse "post fonctionne"
+app.post("/api/coworkings", (req, res) => {
+  console.log(req.body)
+  // Ajouter le coworking dans le tableau, en automatisant le génération d'un id
+  // On récupère le dernier élément du tableau et on ajoute +1 à son id
+  // let coworking = req.body
+  const newId = mockCoworkings[mockCoworkings.length - 1].id + 1
+  //... SPREAD OPERATOR
+  let coworking = { id: newId, ...req.body }
+  mockCoworkings.push(coworking)
+
+  // On renvoie un objet qui contient les propriétés message et data
+  let result = { msg: "Le coworking a bien été ajouté", data: coworking }
+  res.json(result)
 })
 
 app.listen(port, () => {
