2581ba33ffcf1e6d8588863231a816cdc555c5d2
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-12-04 14:52:12 +0100

    Update coworkings delete endpoint with 2 promises

--- routes/coworkingRoutes.js
+++ routes/coworkingRoutes.js
@@ -46,34 +46,50 @@ router
   })
 
   .put((req, res) => {
-    Coworking.update(req.body, {
-      where: { id: req.params.id },
-    })
+    Coworking.findByPk(req.params.id)
       .then((coworking) => {
-        if (coworking > 0) {
-          res.json({ message: `Le coworking a bien été modifié`, data: coworking })
-        } else {
-          res.json({ message: "Le coworking n'existe pas" })
-        }
+        Coworking.update(req.body, {
+          where: { id: req.params.id },
+        })
+          .then(() => {
+            if (coworking > 0) {
+              res.json({ message: `Le coworking a bien été modifié`, data: coworking })
+            } else {
+              res.json({ message: "Le coworking n'existe pas" })
+            }
+          })
+          .catch((error) => {
+            res.json({ message: `La mise à jour a échoué`, data: error.message })
+          })
       })
-      .catch((error) => {
+      .catch(() => {
         res.json({ message: `Une erreur est survenue`, data: error.message })
       })
   })
 
   .delete((req, res) => {
-    Coworking.destroy({
-      where: { id: req.params.id },
-    })
+    // A. On vérifie que l'id passé en req.params.id renvoie bien une ligne de notre table.
+    Coworking.findByPk(req.params.id)
       .then((coworking) => {
+        // B. Si un coworking correspond à l'id alors on exécute la méthode destroy()
         if (coworking) {
-          res.json({ message: "Le coworking a bien été supprimé", data: coworking })
+          Coworking.destroy({ where: { id: req.params.id } })
+            // C. Si le coworking est bien supprimé, on affiche un message avec comme data le coworking récupéré dans le .findByPk()
+            .then(() => {
+              res.json({ mesage: `Le coworking a bien été supprimé.`, data: coworking })
+            })
+            // D. Si la suppression a échoué, on retourne une réponse à POSTMAN
+            .catch((error) => {
+              res.json({ mesage: `La suppression a échoué.`, data: error.message })
+            })
         } else {
-          res.json({ message: "Le coworking n'existe pas" })
+          // B Si aucun coworking ne correspond à l'id alors on retourne une réponse à POSTMAN
+          res.json({ mesage: `Aucun coworking trouvé.` })
         }
       })
       .catch((error) => {
-        res.json({ message: `Une erreur est survenue`, data: error.message })
+        // E. Si une erreur est survenue dès le findByPk, on retourne une réponse à POSTMAN
+        res.json({ mesage: `La requête n'a pas aboutie.` })
       })
   })
 
