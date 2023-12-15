ce014cd1762c99ea86a6d06d88f51153aca57aec
Mathieu Etourneaud <mathieu.etourneaud@lapiscine.pro>
2023-12-15 15:01:14 +0100

    Add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 // const cookieParser = require("cookie-parser")
 const app = express()
 const port = 3000
@@ -8,6 +9,7 @@ const port = 3000
 
 app.use(express.json())
 app.use(morgan("dev"))
+app.use(cors())
 // app.use(cookieParser())
 
 app.get("/", (req, res) => {
